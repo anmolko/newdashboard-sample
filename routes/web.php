61ab53b16b736d2f5163382fb23481e8cb1e3736
anmolko <anmol@canosoft.com.np>
2022-03-13 14:58:40 +0545

    project setup

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Auth::routes([
+    'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
+]);
+
+Route::any('/register', function() {
+    abort(404);
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::group(['prefix' => 'auth', 'middleware' => ['auth']], function () {
+    Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
+
+    //signed-in user routes
+//    Route::get('/profile', 'App\Http\Controllers\UserController@profile')->name('profile');
+//    Route::put('/profile/{id}/update', 'App\Http\Controllers\UserController@update')->name('user.update');
+//    Route::put('/profileimage/{id}/update', 'App\Http\Controllers\UserController@imageupdate')->name('user.imageupdate');
+//    Route::put('/profile/password', 'App\Http\Controllers\UserController@profilepassword')->name('user.password');
+    //end of signed-in user routes
+
+
+});
+
+
