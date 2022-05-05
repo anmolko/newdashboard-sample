736a1f1ea0cd655be7406c97944e536e32ab17c4
anmolko <anmol@canosoft.com.np>
2022-05-05 13:12:34 +0545

    user removal and status change update

--- public/assets/backend/custom_js/user-mgm.js
+++ public/assets/backend/custom_js/user-mgm.js
@@ -77,7 +77,7 @@ $('#user-add-button').on('click', function(e) {
                     '</a> ' +
                     '<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuLink2"> ' +
                     '<li><a class="dropdown-item" href="'+slug+'"><i class="ri-eye-line me-2 align-middle"></i>Profile</a></li> ' +
-                    '<li><a class="dropdown-item cs-role-change" id="cs-role-change-'+response.user.id+'" cs-user-role="'+response.user.user_type+'" cs-user-id="'+response.user.id+'" cs-update-route="'+user_type_update+'"><i class="ri-shield-user-line me-2 align-middle"></i>User Type</a></li> ' +
+                    '<li><a class="dropdown-item cs-role-change" id="cs-role-change-'+response.user.id+'" cs-user-role="'+response.user.user_type+'" cs-user-id="'+response.user.id+'" cs-update-route="'+user_type_update+'"><i class="ri-shield-user-line me-2 align-middle"></i>User Type</a></li>' +
                     '<li><a class="dropdown-item cs-user-remove" cs-delete-route="'+user_remove+'"><i class="ri-delete-bin-6-line me-2 align-middle"></i>Delete</a></li> ' +
                     '</ul>' +
                     '</div>' +
@@ -284,7 +284,7 @@ function statusupdate(url,status){
     });
 }
 
-$('.cs-role-change').on('click', function() {
+$(document).on('click','.cs-role-change', function () {
     var userrole    = $(this).attr('cs-user-role');
     var id          = $(this).attr('cs-user-id');
     var updateroute = $(this).attr('cs-update-route');
@@ -294,7 +294,7 @@ $('.cs-role-change').on('click', function() {
     $('#changestatus').modal('show');
 });
 
-$('#user-role-change').on('click', function(e) {
+$(document).on('click','#user-role-change', function () {
     var usertype       = $('#user_type_change').val();
     var id             = $('#userid_role').val();
     var url            = $(this).attr('cs-update-role');
@@ -368,3 +368,100 @@ $('#user-role-change').on('click', function(e) {
 
 });
 
+$(document).on('click','.cs-user-remove', function (e) {
+    e.preventDefault();
+    var form = $('#deleted-form');
+    var action = $(this).attr('cs-delete-route');
+    form.attr('action',action);
+    var url = form.attr('action');
+    var form_data = form.serialize();
+    Swal.fire({
+        imageUrl: "/assets/backend/images/canosoft-logo.png",
+        imageHeight: 60,
+        html: '<div class="mt-2">' +
+            '<lord-icon src="https://cdn.lordicon.com/tdrtiskw.json"' +
+            ' trigger="loop" colors="primary:#f06548,secondary:#f7b84b" ' +
+            'style="width:120px;height:120px"></lord-icon>' +
+            '<div class="mt-4 pt-2 fs-15">' +
+            '<h4>Are your sure? </h4>' +
+            '<p class="text-muted mx-4 mb-0">' +
+            'Removing user will erase all of their activiy and data</p>' +
+            '</div>' +
+            '</div>',
+        showCancelButton: !0,
+        confirmButtonClass: "btn btn-primary w-xs me-2 mt-2",
+        cancelButtonClass: "btn btn-danger w-xs mt-2",
+        confirmButtonText: "Yes!",
+        buttonsStyling: !1,
+        showCloseButton: !0
+    }).then(function(t)
+    {
+        t.value
+            ?
+            $.post( url, form_data)
+                .done(function(response) {
+                    console.log(response);
+                    if(response.status == "success") {
+                        Swal.fire({
+                            imageUrl: "/assets/backend/images/canosoft-logo.png",
+                            imageHeight: 60,
+                            html: '<div class="mt-2">' +
+                                '<lord-icon src="https://cdn.lordicon.com/lupuorrc.json"' +
+                                'trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px">' +
+                                '</lord-icon>' +
+                                '<div class="mt-4 pt-2 fs-15">' +
+                                '<h4>Success !</h4>' +
+                                '<p class="text-muted mx-4 mb-0">' + response.message +'</p>' +
+                                '</div>' +
+                                '</div>',
+                            timerProgressBar: !0,
+                            timer: 2e3,
+                            showConfirmButton: !1
+                        });
+                        var user_block = '#user-block-num-'+response.id;
+                        var user_cover = '#user-block-cover-'+response.id;
+                        $(user_block).css('background','#ffa5a5');
+                        setTimeout(function() {
+                            $(user_cover).remove();
+                        }, 3800);
+                    }else{
+                        Swal.fire({
+                            imageUrl: "/assets/backend/images/canosoft-logo.png",
+                            imageHeight: 60,
+                            html: '<div class="mt-2">' +
+                                '<lord-icon src="https://cdn.lordicon.com/tdrtiskw.json"' +
+                                ' trigger="loop" colors="primary:#f06548,secondary:#f7b84b" ' +
+                                'style="width:120px;height:120px"></lord-icon>' +
+                                '<div class="mt-4 pt-2 fs-15">' +
+                                '<h4>Oops...! </h4>' +
+                                '<p class="text-muted mx-4 mb-0">' + response.message +'</p>' +
+                                '</div>' +
+                                '</div>',
+                            timerProgressBar: !0,
+                            timer: 3000,
+                            showConfirmButton: !1
+                        });
+                    }
+                })
+                .fail(function(response){
+                    console.log(response)
+                })
+
+            :
+            t.dismiss === Swal.DismissReason.cancel &&
+            Swal.fire({
+                title: "Cancelled",
+                text: "User was not removed.",
+                icon: "error",
+                confirmButtonClass: "btn btn-primary mt-2",
+                buttonsStyling: !1
+            });
+    });
+
+
+
+
+
+
+
+})
--- resources/views/backend/user/alluser.blade.php
+++ resources/views/backend/user/alluser.blade.php
@@ -179,107 +179,10 @@
 
     <script src="{{asset('assets/backend/custom_js/user-mgm.js')}}"></script>
 
-    <script type="text/javascript">
-
-        $(document).on('click','.cs-user-remove', function (e) {
-            e.preventDefault();
-            var form = $('#deleted-form');
-            var action = $(this).attr('cs-delete-route');
-            form.attr('action',action);
-            var url = form.attr('action');
-            var form_data = form.serialize();
-            Swal.fire({
-                imageUrl: "/assets/backend/images/canosoft-logo.png",
-                imageHeight: 60,
-                html: '<div class="mt-2">' +
-                    '<lord-icon src="https://cdn.lordicon.com/tdrtiskw.json"' +
-                    ' trigger="loop" colors="primary:#f06548,secondary:#f7b84b" ' +
-                    'style="width:120px;height:120px"></lord-icon>' +
-                    '<div class="mt-4 pt-2 fs-15">' +
-                    '<h4>Are your sure? </h4>' +
-                    '<p class="text-muted mx-4 mb-0">' +
-                    'Removing user will erase all of their activiy and data</p>' +
-                    '</div>' +
-                    '</div>',
-                showCancelButton: !0,
-                confirmButtonClass: "btn btn-primary w-xs me-2 mt-2",
-                cancelButtonClass: "btn btn-danger w-xs mt-2",
-                confirmButtonText: "Yes!",
-                buttonsStyling: !1,
-                showCloseButton: !0
-            }).then(function(t)
-            {
-                t.value
-                    ?
-                    $.post( url, form_data)
-                        .done(function(response) {
-                            console.log(response);
-                            if(response.status == "success") {
-                                Swal.fire({
-                                    imageUrl: "/assets/backend/images/canosoft-logo.png",
-                                    imageHeight: 60,
-                                    html: '<div class="mt-2">' +
-                                        '<lord-icon src="https://cdn.lordicon.com/lupuorrc.json"' +
-                                        'trigger="loop" colors="primary:#0ab39c,secondary:#405189" style="width:120px;height:120px">' +
-                                        '</lord-icon>' +
-                                        '<div class="mt-4 pt-2 fs-15">' +
-                                        '<h4>Success !</h4>' +
-                                        '<p class="text-muted mx-4 mb-0">' + response.message +'</p>' +
-                                        '</div>' +
-                                        '</div>',
-                                    timerProgressBar: !0,
-                                    timer: 2e3,
-                                    showConfirmButton: !1
-                                });
-                                var user_block = '#user-block-num-'+response.id;
-                                var user_cover = '#user-block-cover-'+response.id;
-                                $(user_block).css('background','#ffa5a5');
-                                setTimeout(function() {
-                                    $(user_cover).remove();
-                                }, 3800);
-                            }else{
-                                Swal.fire({
-                                    imageUrl: "/assets/backend/images/canosoft-logo.png",
-                                    imageHeight: 60,
-                                    html: '<div class="mt-2">' +
-                                        '<lord-icon src="https://cdn.lordicon.com/tdrtiskw.json"' +
-                                        ' trigger="loop" colors="primary:#f06548,secondary:#f7b84b" ' +
-                                        'style="width:120px;height:120px"></lord-icon>' +
-                                        '<div class="mt-4 pt-2 fs-15">' +
-                                        '<h4>Oops...! </h4>' +
-                                        '<p class="text-muted mx-4 mb-0">' + response.message +'</p>' +
-                                        '</div>' +
-                                        '</div>',
-                                    timerProgressBar: !0,
-                                    timer: 3000,
-                                    showConfirmButton: !1
-                                });
-                            }
-                        })
-                        .fail(function(response){
-                            console.log(response)
-                        })
-
-                    :
-                    t.dismiss === Swal.DismissReason.cancel &&
-                    Swal.fire({
-                        title: "Cancelled",
-                        text: "User was not removed.",
-                        icon: "error",
-                        confirmButtonClass: "btn btn-primary mt-2",
-                        buttonsStyling: !1
-                    });
-            });
+{{--    <script type="text/javascript">--}}
 
 
 
 
-
-
-
-        })
-
-
-
-    </script>
+{{--    </script>--}}
 @endsection
