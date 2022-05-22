eefcfd96ee281c4629aeef071ad3effa194b48dd
suraj <suraj.canosoft@gmail.com>
2022-05-22 12:45:15 +0545

    heading changed

--- resources/views/backend/blog/create.blade.php
+++ resources/views/backend/blog/create.blade.php
@@ -24,7 +24,7 @@
             <div class="row">
                 <div class="col-12">
                     <div class="page-title-box d-sm-flex align-items-center justify-content-between">
-                        <h4 class="mb-sm-0">Create Blog</h4>
+                        <h4 class="mb-sm-0">Blog</h4>
 
                         <div class="page-title-right">
                             <ol class="breadcrumb m-0">
