f71951975d4ce9789e9e9a9aa451cb55d3b1feb3
anmolko <anmol@canosoft.com.np>
2022-06-02 10:56:02 +0545

    career migration update

---
+++ database/migrations/2022_05_25_072959_create_careers_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCareersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('position')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('feature_image')->nullable();
+            $table->enum('type',['full_time','part_time'])->nullable();
+            $table->string('start_date')->nullable();
+            $table->string('end_date')->nullable();
+            $table->string('salary')->nullable();
+            $table->enum('status',['active','inactive'])->nullable();
+            $table->string('meta_title')->nullable();
+            $table->text('meta_tags')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('updated_by')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('careers');
+    }
+}
