d2b2ae3bb3b4348b55962d0879a43693c67d9b1b
anmolko <anmol@canosoft.com.np>
2022-06-16 13:54:56 +0545

    custom js for dashboard

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -403,7 +403,6 @@
     <script>
         @if(Session::has('success'))
         var message ="{{ session('success') }}";
-        // Toastify({ newWindow: !0, text: message, gravity: 'top', position: 'center', stopOnFocus: !0, duration: 3000, close: "close",className: "bg-success" }).showToast();
         Swal.fire({
             imageUrl: "/assets/backend/images/canosoft-logo.png",
             imageHeight: 60,
@@ -484,130 +483,8 @@
             showConfirmButton: !1
         });
         @endif
-        var id  = "{{$setting_data->id}}";
-
-        $(document).on('click','#change-theme-mode', function (e) {
-            e.preventDefault();
-            var mode = $('html').attr('data-layout-mode');
-            var formData = new FormData();
-            formData.append('setting_id', id);
-            formData.append('mode', mode);
-            var url = "/auth/dashboard-settings/theme-mode/"
-            $.ajax({
-                type : 'POST',
-                url : url,
-                headers: {
-                    'X-CSRF-Token': $('meta[name="_token"]').attr('content')
-                },
-                cache: false,
-                contentType: false,
-                processData: false,
-                data : formData,
-                success: function(response){
-                    if(response.status=='success') {
-                        console.log(response.mode);
-                        // $("#user-list").prepend(block);
-                    }
-
-                }, error: function(response) {
-                    console.log(response);
-                }
-            });
-        });
-
-        function sendMarkRequest(id = null, name = null) {
-            return $.ajax("{{ route('markNotification') }}", {
-                method: 'POST',
-                headers: {
-                    'X-CSRF-Token': $('meta[name="_token"]').attr('content')
-                },
-                data: {
-                    id,
-                    name
-                }
-            });
-        }
-        $(function() {
-            $('.mark-as-read').click(function() {
-                var id      = $(this).data('id');
-                var type    = $(this).data('name');
-                var value   = $(this).data('type');
-                let request = sendMarkRequest(id,value);
-                var div     = '#notification-'+type+id;
-                var mark    = '#all-read-'+type;
-                var count_type = '#'+type+'-count';
-                var holder = '#main-'+type+'-holder';
-
-                request
-                    .done(function(response) {
-                        console.log(response);
-                        $("#top-unread").html('');
-                        $("#top-unread").append(response.unread+'<span class="visually-hidden">unread messages</span>');
-                        $("#new-unread").html('');
-                        if(type == 'service'){
-                            $(count_type).html('');
-                            $(count_type).append('Service (' + response.service_num +')');
-                        }else if(type == 'career'){
-                            $(count_type).html('');
-                            $(count_type).append('Career (' + response.career_num +')');
-                        }
-                        $("#new-unread").append(response.unread+' New');
-                        $(div).remove();
-                        if(response.service_num == 0 || response.career_num == 0){
-                            $(mark).remove();
-                            var replacement = '  <div class="w-25 w-sm-50 pt-3 mx-auto">' +
-                                '<img src="/assets/backend/images/svg/bell.svg" class="img-fluid" alt="user-pic">' +
-                            '</div>' +
-                            '<div class="text-center pb-5 mt-2">'+
-                            '<h6 class="fs-18 fw-semibold lh-base">Hey! You have no '+type+' notifications </h6>'+
-                            '</div>';
-                            $(holder).append(replacement);
-                        }
-                })
-                 .fail(function(response){
-                        console.log(response)
-                 });
-            });
-
-            $('.mark-all').click(function() {
-
-                var name       = $(this).data('name');
-                var type       = $(this).data('id');
-                let request    = sendMarkRequest(null,name);
-                var mark       = '#all-read-'+type;
-                var holder     = '#main-'+type+'-holder';
-                var container  = 'div.notify-item-'+type;
-                var count_type = '#'+type+'-count';
-
-                request
-                    .done(function(response) {
-                        console.log(response);
-                        var unread = (response.service_num + response.career_num);
-                        $("#top-unread").html('');
-                        $("#top-unread").append(unread +' <span class="visually-hidden">unread messages</span>');
-                        $("#new-unread").html('');
-                        if(type == 'service'){
-                            $(count_type).html('');
-                            $(count_type).append('Service (' + response.service_num +')');
-                        }else if(type == 'career'){
-                            $(count_type).html('');
-                            $(count_type).append('Career (' + response.career_num +')');
-                        }
-                        $("#new-unread").append(unread+' New');
-                        $(container).remove();
-                        $(mark).remove();
-                        var replacement = '  <div class="w-25 w-sm-50 pt-3 mx-auto">' +
-                            '<img src="/assets/backend/images/svg/bell.svg" class="img-fluid" alt="user-pic">' +
-                            '</div>' +
-                            '<div class="text-center pb-5 mt-2">'+
-                            '<h6 class="fs-18 fw-semibold lh-base">Hey! You have no '+type+' notifications </h6>'+
-                            '</div>';
-                        $(holder).append(replacement);
-                    })
-            });
-        });
-
-
-    </script>
+        var set_id  = "{{$setting_data->id}}";
+</script>
+<script src="{{asset('assets/backend/custom_js/dash-custom.js')}}"></script>
 </body>
 </html>
