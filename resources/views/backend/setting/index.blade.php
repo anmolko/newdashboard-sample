d5b2ae6cd4d7252e91240068a423a31c5ef0d4be
suraj <suraj.canosoft@gmail.com>
2022-06-20 15:35:06 +0545

    logo px defined

--- resources/views/backend/setting/index.blade.php
+++ resources/views/backend/setting/index.blade.php
@@ -163,7 +163,7 @@
                                                         @if(!empty($settings->logo))
                                                             <img src="{{asset('images/settings/'.$settings->logo)}}" class="img-thumbnail" width="300" alt="One Slide">
                                                         @endif
-                                                        <p class="text-muted">Add the logo for frontend and backend display.</p>
+                                                        <p class="text-muted">Add the logo (410 * 100 px) for frontend and backend display.</p>
                                                         <input class="form-control" name="logo" id="main-logo-input" type="file" accept="image/png, image/jpeg">
                                                     </div>
                                                     <div class="mb-4">
@@ -171,6 +171,8 @@
                                                         @if(!empty($settings->logo_white))
                                                             <img src="{{asset('images/settings/'.$settings->logo_white)}}" class="img-thumbnail" width="300" alt="Two Slide">
                                                         @endif
+                                                        <p class="text-muted">Add the white logo (410 * 100 px) for frontend display.</p>
+
                                                         <input class="form-control" name="logo_white" id="white-logo-input" type="file" accept="image/png, image/jpeg">
                                                     </div>
                                                     <div class="mb-4">
@@ -178,6 +180,7 @@
                                                         @if(!empty($settings->favicon))
                                                             <img src="{{asset('images/settings/'.$settings->favicon)}}" class="img-thumbnail" width="50" alt="Three Slide">
                                                         @endif
+                                                        <p class="text-muted">Add the favicon(32 * 32px).</p>
 
                                                         <input class="form-control" name="favicon" id="favicon-logo-input" type="file" accept="image/png, image/jpeg">
                                                     </div>
